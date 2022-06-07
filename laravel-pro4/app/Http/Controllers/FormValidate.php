f360e5bbd7ccc510f85c636ce23b07c086743052
Dua-Alsafasfeh <dua.alsafasfeh@gmail.com>
2022-06-07 01:31:18 -0700

    relationship

---
+++ laravel-pro4/app/Http/Controllers/FormValidate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use App\Models\Newpost;
+use Illuminate\Http\Request;
+
+class FormValidate extends Controller
+{
+    public function form_validate(Request $request ){
+        $request->validate([
+            'firstname'=>'required|alpha',
+            'email'=>'required|email',
+            'password'=>'required|min:8|regex:/^.(?=.{3,})(?=.[a-zA-Z])(?=.[0-9])(?=.[\d\x])(?=.[!$#%]).$/',
+            'phonenumber'=>'numeric|digits_between:9,11'
+        ]);
+        return $request->input();//to show data in json format
+    }
+    public function one_relation(){
+        dd (User::find(1)->post);
+    }
+    public function viewComment(){
+        return newpost::find(1)->comment;
+    }
+}
---
+++ laravel-pro4/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormValidate;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::controller(UserController::class)->group (function(){
+    Route::get('/index','createuser');
+}) ;
+Route::post('form', [FormValidate::class, 'form_validate']);
+Route::view('show','form');
+Route::get('/posts',[FormValidate::class, 'one_relation']);
+Route::get('/comment',[FormValidate::class, 'viewComment']);
