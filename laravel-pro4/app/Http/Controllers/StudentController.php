712483c9b2efdfb50525a126b5e60848147a71d9
Dua-Alsafasfeh <dua.alsafasfeh@gmail.com>
2022-06-19 22:27:46 -0700

    react test 1

--- laravel-pro4/app/Http/Controllers/StudentController.php
+++ laravel-pro4/app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\student;
+use App\Models\cources;
 
 use Illuminate\Http\Request;
 
@@ -10,5 +11,11 @@ class StudentController extends Controller
     public function viewStudent(){
         $student = Student::all();
         return view('/acad', compact('student'));
+    }
+    public function manyToMany_relation(){
+        $student = student::find(2);
+        $course = cources::find([3, 4]);
+        $student->callCourses()->attach($course);
+        return 'Success';
     }
 }
